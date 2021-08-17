ee7331c5d07a1e0172040bd9f7b5ffb229ad41e0
SylwiaLeung <longdepati@gmail.com>
2021-08-17 10:41:36 +0200

    accepted andrzej's changes to introduce method

--- Gittie.cs
+++ Gittie.cs
@@ -1,10 +1,6 @@
 public void introduce()
 {
-<<<<<<< HEAD
     Console.WriteLine("Hello, I'm Andrzej!");
-=======
-    Console.WriteLine("Hello, I'm Sylwia!");
->>>>>>> 2f61f7b4e686f73bd870eee46fe2ab1e0fa1d854
     
 }
 public void joke()
